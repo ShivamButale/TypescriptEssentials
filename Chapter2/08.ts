43e49fbe73541293381835ffc871c782b0db3777
Shivam Butale <shivbutale22@gmail.com>
2022-03-25 13:42:28 +0530

    Update 08.ts

--- Chapter2/08.ts
+++ Chapter2/08.ts
@@ -21,12 +21,12 @@ This allows us to call this function with variable number of arguments.
 operation('+', 1,2,3) would return 6
 operation('+',10,20) would return 30
 
-Example 2) Using it to
+Example 2) Using it to add elements to an array 
 const source = [4,5,6]
 const target = [1,2,3, ..source,7,8]
 Now target is this array: [1,2,3,4,5,6,7,8]
 
-Example 3)
+Example 3) Use it to append it to another array 
 const a = [1,2,3]
 const b = [4,5,6]
 a.push(...b)
