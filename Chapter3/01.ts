f24c2e0a2bb032f0b345f4900a52e2561aeb5b9d
Shivam Butale <shivbutale22@gmail.com>
2022-03-28 16:26:49 +0530

    Create 01.ts

---
+++ Chapter3/01.ts
@@ -0,0 +1,9 @@
+This video discusses the data types
+ECMAScript5 Supports these primitive data types
+    boolean
+    number
+    string
+    null/undefined
+    object
+
+P.s. Objects are collections of key-value pairs
