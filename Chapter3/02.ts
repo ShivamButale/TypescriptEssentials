ed580c3e03b6d8cf0b1872663f63bd63b5b1850d
Shivam Butale <shivbutale22@gmail.com>
2022-03-30 15:00:55 +0530

    Create 02.ts

---
+++ Chapter3/02.ts
@@ -0,0 +1,11 @@
+This video discusses the concept of type inference.
+It means typescript infers the data types of variables, return types, etc.
+
+Example:
+If you declare this function :
+function abc(a: string, b: string) {
+    return a+b;
+}
+
+Now if you hover over the function abc, you will see typescript shows its return type as string.
+So, typescript has inferred the return type of this function.
