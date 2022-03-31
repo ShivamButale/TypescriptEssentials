dad07bcd64f76a2f89a6c7bda2fd4638e686dc7b
Shivam Butale <shivbutale22@gmail.com>
2022-03-31 19:31:15 +0530

    Create 03.ts

---
+++ Chapter3/03.ts
@@ -0,0 +1,6 @@
+This video discusses specifying javascript types.
+It basically means we can explicitly specify the types of variables, return types of functions, etc in typescript.
+Example:
+function abc(a: string, b: string): string {
+    return a+b;
+}
