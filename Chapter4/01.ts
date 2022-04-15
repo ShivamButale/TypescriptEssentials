a5de9c30b3ce97ff0d6c53848126e01f65815540
Shivam Butale <shivbutale22@gmail.com>
2022-04-15 18:50:42 +0530

    Create 01.ts

---
+++ Chapter4/01.ts
@@ -0,0 +1,8 @@
+This video discusses the concept of interfaces.
+Typescript provides interfaces which can be used to define custom types.
+
+Here is an example:
+interface Demo {
+    name: string;
+    exists?: boolean;
+}
